267a41aca3ae0c725085bcd9492404bbb9ecbe32
B0FM2G <niki.lancz@gmail.com>
2023-10-04 20:36:33 +0200

    3zh
    B0FM2G

--- src/utils/distributions.py
+++ src/utils/distributions.py
@@ -133,3 +133,103 @@ class CauchyDistribution:
 
     def mvsk(self):
         raise Exception("Moments undefined")
+
+class LogisticDistribution:
+    def __init__(self, rand, location, scale):
+        self.rand = rand
+        self.location = location
+        self.scale = scale
+
+    def pdf(self,x):
+        exponent = -(x - self.location) / self.scale
+        pdf_value = math.exp(exponent)/(self.scale*(1 + math.exp(exponent)) ** 2)
+        return pdf_value
+
+    def cdf(self, x):
+        exponent = -(x - self.location) / self.scale
+        cdf_value = 1 / (1 + math.exp(exponent))
+        return cdf_value
+
+    def ppf(self, p):
+        if 0 < p < 1:
+            ppf_value = self.location - self.scale * math.log(1 / p - 1)
+            return ppf_value
+        else:
+            raise ValueError("p-nek 0 és 1 között kell lennie")
+
+    def gen_rand(self):
+        u = self.rand.random()
+        rand_value = self.location - self.scale * math.log(1 / u - 1)
+        return rand_value
+
+    def mean(self):
+        if self.scale == 0:
+            raise Exception("Moment undefined")
+        return self.location
+
+    def variance(self):
+        if self.scale == 0:
+            raise Exception("Moment undefined")
+        variance = (math.pi ** 2 * self.scale ** 2) / 3
+        return variance
+
+    def skewness(self):
+        if self.scale == 0:
+            raise Exception("Moment undefined")
+        skewness = 0
+        return skewness
+
+    def ex_kurtosis(self):
+        if self.scale == 0:
+            raise Exception("Moment undefined")
+        ex_kurtosis = 1.2
+        return ex_kurtosis
+
+    def mvsk(self):
+        if self.scale == 0:
+            raise Exception("Moment undefined")
+        return [self.mean(), self.variance(), self.skewness(), self.ex_kurtosis()]
+
+import scipy.special as scp
+class ChiSquaredDistribution:
+    def __init__(self, rand, dof):
+        self.rand = rand
+        self.dof = dof
+
+    def pdf(self,x):
+        if x >= 0:
+            pdf_value = (1 / (2 ** (self.dof / 2) * math.gamma(self.dof / 2))) * x ** (self.dof / 2 - 1) * math.exp(
+                -x / 2)
+            return pdf_value
+        else:
+            return 0
+
+    def cdf(self, x):
+        if x >= 0:
+            cdf_value = scp.gammainc(self.dof/2, x/2)
+            return cdf_value
+        else:
+            return 0
+    def ppf(self, p):
+        if 0 <= p <= 1:
+            ppf_value = 2 * scp.gammaincinv(self.dof/2, p)
+            return ppf_value
+        else:
+            raise ValueError("p-nek 0 és 1 között kell lennie")
+
+    def gen_rand(self):
+        return sum(random.gauss(0, 1) ** 2 for _ in range(int(self.dof)))
+
+    def mean(self):
+        return self.dof
+
+    def variance(self):
+        return 2 * self.dof
+
+    def skewness(self):
+        return (8 / self.dof) ** 0.5
+    def ex_kurtosis(self):
+        return 12 / self.dof
+
+    def mvsk(self):
+        return [self.mean(), self.variance(), self.skewness(), self.ex_kurtosis()]
