4cdaf09a3a0d0b67f4c8e477e19c46e98e2e9e3a
B0FM2G <niki.lancz@gmail.com>
2023-11-08 20:19:00 +0100

    B0FM2G
    7zh

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -1,5 +1,9 @@
 import statsmodels.api as sm
 import pandas as pd
+import numpy as np
+from scipy.stats import t
+from scipy.stats import f
+
 class LinearRegressionSM:
     def __init__(self, left_hand_side, right_hand_side):
         self.left_hand_side = left_hand_side
@@ -28,3 +32,61 @@ class LinearRegressionSM:
         ak = '{:.2e}'.format(round(self._model.aic, 2))
         by = '{:.2e}'.format(round(self._model.bic, 2))
         return f'Adjusted R-squared: {ars}, Akaike IC: {ak}, Bayes IC: {by}'
+
+class LinearRegressionNP:
+    def __init__(self, left_hand_side, right_hand_side):
+        self.left_hand_side = left_hand_side
+        self.right_hand_side = right_hand_side
+        self._model = None
+
+    def fit(self):
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        beta = np.linalg.inv(X.T @ X) @ X.T @ y
+        self.beta = beta
+
+    def get_params(self):
+        return pd.Series(self.beta, name='Beta coefficients')
+
+    def get_pvalues(self):
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        n, k = X.shape
+        beta = self.beta
+        H = X @ np.linalg.inv(X.T @ X) @ X.T
+        residuals = y - X @ beta
+        residual_variance = (residuals @ residuals) / (n - k)
+        standard_errors = np.sqrt(np.diagonal(residual_variance * np.linalg.inv(X.T @ X)))
+        t_statistics = beta / standard_errors
+        df = n - k
+        p_values = [2 * (1 - t.cdf(abs(t_stat), df)) for t_stat in t_statistics]
+        p_values = pd.Series(p_values, name="P-values for the corresponding coefficients")
+        return p_values
+
+    def get_wald_test_result(self, R):
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        beta = self.beta
+        residuals = y - X @ beta
+        r_matrix = np.array(R)
+        r = r_matrix @ beta
+        n = len(self.left_hand_side)
+        m, k = r_matrix.shape
+        sigma_squared = np.sum(residuals ** 2) / (n - k)
+        H = r_matrix @ np.linalg.inv(X.T @ X) @ r_matrix.T
+        wald = (r.T @ np.linalg.inv(H) @ r) / (m * sigma_squared)
+        p_value = 1 - f.cdf(wald, dfn=m, dfd=n - k)
+        return f'Wald: {wald:.3f}, p-value: {p_value:.3f}'
+
+    def get_model_goodness_values(self):
+        X = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y = self.left_hand_side
+        n, k = X.shape
+        beta = self.beta
+        y_pred = X @ beta
+        ssr = np.sum((y_pred - np.mean(y)) ** 2)
+        sst = np.sum((y - np.mean(y)) ** 2)
+        centered_r_squared = ssr / sst
+        adjusted_r_squared = 1 - (1 - centered_r_squared) * (n - 1) / (n - k)
+        result = f"Centered R-squared: {centered_r_squared:.3f}, Adjusted R-squared: {adjusted_r_squared:.3f}"
+        return result
