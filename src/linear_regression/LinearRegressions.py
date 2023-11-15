21413481d8774b629670a53356220d7e762d5796
B0FM2G <niki.lancz@gmail.com>
2023-11-15 20:55:55 +0100

    8zh
    B0FM2G

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -90,3 +90,113 @@ class LinearRegressionNP:
         adjusted_r_squared = 1 - (1 - centered_r_squared) * (n - 1) / (n - k)
         result = f"Centered R-squared: {centered_r_squared:.3f}, Adjusted R-squared: {adjusted_r_squared:.3f}"
         return result
+
+import numpy as np
+import pandas as pd
+from scipy.stats import t, f
+
+class LinearRegressionGLS:
+    def __init__(self, left_hand_side, right_hand_side):
+        self.left_hand_side = left_hand_side.values
+        self.right_hand_side = right_hand_side.values
+
+    def fit(self):
+
+        X_ols = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        beta_ols = np.linalg.inv(X_ols.T @ X_ols) @ X_ols.T @ self.left_hand_side
+        residuals = self.left_hand_side - X_ols @ beta_ols
+
+        X_omega = np.column_stack(
+                (np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y_omega = np.log(residuals ** 2)
+        beta_omega = np.linalg.inv(X_omega.T @ X_omega) @ X_omega.T @ y_omega
+
+        V_inv_diag = 1 / np.sqrt(np.exp(X_omega @ beta_omega))
+        V_inv = np.diag(V_inv_diag)
+
+        X_gls = np.linalg.inv(np.sqrt(V_inv)) @ X_ols
+        y_gls = np.linalg.inv(np.sqrt(V_inv)) @ self.left_hand_side
+
+        beta_gls = np.linalg.inv(X_gls.T @ X_gls) @ X_gls.T @ y_gls
+
+        self.beta_params = beta_gls
+
+    def get_params(self):
+        return pd.Series(self.beta_params, name='Beta coefficients')
+
+    def get_pvalues(self):
+        X_ols = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        beta_ols = np.linalg.inv(X_ols.T @ X_ols) @ X_ols.T @ self.left_hand_side
+        residuals = self.left_hand_side - X_ols @ beta_ols
+
+        X_omega = np.column_stack(
+            (np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y_omega = np.log(residuals ** 2)
+        beta_omega = np.linalg.inv(X_omega.T @ X_omega) @ X_omega.T @ y_omega
+
+        V_inv_diag = 1 / np.sqrt(np.exp(X_omega @ beta_omega))
+        V_inv = np.diag(V_inv_diag)
+
+        X_gls = np.linalg.inv(np.sqrt(V_inv)) @ X_ols
+        y_gls = np.linalg.inv(np.sqrt(V_inv)) @ self.left_hand_side
+
+        beta_gls = np.linalg.inv(X_gls.T @ X_gls) @ X_gls.T @ y_gls
+        dof = len(self.left_hand_side) - X_ols.shape[1]
+        t_stat = self.beta_params / np.sqrt(np.diag(np.linalg.inv(X_gls.T @ X_gls)))
+        p_values = pd.Series([min(value, 1 - value) * 2 for value in t.cdf(-np.abs(t_stat), df=dof)],
+                             name='P-values for the corresponding coefficients')
+        return p_values
+
+    def get_wald_test_result(self, R):
+        X_ols = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        beta_ols = np.linalg.inv(X_ols.T @ X_ols) @ X_ols.T @ self.left_hand_side
+        residuals = self.left_hand_side - X_ols @ beta_ols
+
+        X_omega = np.column_stack(
+            (np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y_omega = np.log(residuals ** 2)
+        beta_omega = np.linalg.inv(X_omega.T @ X_omega) @ X_omega.T @ y_omega
+
+        V_inv_diag = 1 / np.sqrt(np.exp(X_omega @ beta_omega))
+        V_inv = np.diag(V_inv_diag)
+
+        X_gls = np.linalg.inv(np.sqrt(V_inv)) @ X_ols
+        y_gls = np.linalg.inv(np.sqrt(V_inv)) @ self.left_hand_side
+
+        beta_gls = np.linalg.inv(X_gls.T @ X_gls) @ X_gls.T @ y_gls
+        residuals_gls = y_gls - X_gls @ beta_gls
+        r_matrix = np.array(R)
+        r = r_matrix @ beta_gls
+        n = len(self.left_hand_side)
+        m, k = r_matrix.shape
+        sigma_squared = np.sum(residuals_gls ** 2) / (n - k)
+        H = r_matrix @ np.linalg.inv(X_gls.T @ X_gls) @ r_matrix.T
+        wald = (r.T @ np.linalg.inv(H) @ r) / (m * sigma_squared)
+        p_value = 1 - f.cdf(wald, dfn=m, dfd=n - k)
+        return f'Wald: {wald:.3f}, p-value: {p_value:.3f}'
+
+    def get_model_goodness_values(self):
+        X_ols = np.column_stack((np.ones(len(self.right_hand_side)), self.right_hand_side))
+        beta_ols = np.linalg.inv(X_ols.T @ X_ols) @ X_ols.T @ self.left_hand_side
+        residuals = self.left_hand_side - X_ols @ beta_ols
+
+        X_omega = np.column_stack(
+            (np.ones(len(self.right_hand_side)), self.right_hand_side))
+        y_omega = np.log(residuals ** 2)
+        beta_omega = np.linalg.inv(X_omega.T @ X_omega) @ X_omega.T @ y_omega
+
+        V_inv_diag = 1 / np.sqrt(np.exp(X_omega @ beta_omega))
+        V_inv = np.diag(V_inv_diag)
+
+        X_gls = np.linalg.inv(np.sqrt(V_inv)) @ X_ols
+        y_gls = np.linalg.inv(np.sqrt(V_inv)) @ self.left_hand_side
+
+        beta_gls = np.linalg.inv(X_gls.T @ X_gls) @ X_gls.T @ y_gls
+        y_mean = np.mean(self.left_hand_side)
+        y_pred = X_ols @ self.beta_params
+        total_sum_of_squares = np.sum((self.left_hand_side - y_mean) ** 2)
+        residual_sum_of_squares = np.sum((self.left_hand_side - y_pred) ** 2)
+        centered_r_squared = 1 - (residual_sum_of_squares / total_sum_of_squares)
+        adjusted_r_squared = 1 - (residual_sum_of_squares / (len(self.left_hand_side) - X_ols.shape[1])) * (
+                    len(self.left_hand_side) - 1) / total_sum_of_squares
+        return f"Centered R-squared: {centered_r_squared:.3f}, Adjusted R-squared: {adjusted_r_squared:.3f}"
